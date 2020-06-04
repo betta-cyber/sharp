5c34d0105ca87fee1ac81f02fdf4fea8fb5c91b3
betta-cyber <l21828604@gmail.com>
2020-06-04 21:56:55 +0800

    update

--- list.py
+++ list.py
@@ -11,8 +11,8 @@ from utils import redis_c, load_yaml
 class Pipeline:
 
     def __init__(self, target):
-        self.target = load_yaml('rule/detail/%s.yml' % target['type'])
-        self.url = target['url']
+        self.list_obj = load_yaml('rule/list/%s.yml' % target['type'])
+        self.url = self.list_obj['url']
         self.result = {}
         self.content = ""
 
@@ -27,7 +27,9 @@ class Pipeline:
         page = await browser.newPage()
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                                 '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299')
-        # await page.setViewport({'width': 1080, 'height': 960})
+
+        # need get page and pagesize
+        # for loop
         await page.goto(self.url)
         self.content = await page.content()
 
@@ -36,10 +38,12 @@ class Pipeline:
 
     async def analysis(self):
         self.content = BeautifulSoup(self.content, 'html.parser')
-
-        for k, v in self.target.items():
-            self.result[k] = self.get_value(v)
-        print(self.result)
+        list_dom = self.content.find_all('li', class_=self.list_obj['pattern']['list_class'])
+        base_url = "https://www.tc260.org.cn"
+        for i in list_dom:
+            url_dom = i.find('a')
+            url = base_url + url_dom['href']
+            print(url)
 
     def get_value(self, selector):
         try:
@@ -66,7 +70,7 @@ if __name__ == '__main__':
     loop_thread.start()
 
     while True:
-        target = redis_c.rpop("target")
+        target = redis_c.rpop("list")
         if target:
             target = json.loads(target)
             p = Pipeline(target)
--- test.py
+++ test.py
@@ -3,6 +3,9 @@
 import json
 from utils import redis_c, load_yaml
 
-a = {"type": "tc260", "url": "https://www.tc260.org.cn/front/postDetail.html?id=20200527151336"}
+# a = {"type": "tc260", "url": "https://www.tc260.org.cn/front/postDetail.html?id=20200527151336"}
+a = {"type": "tc260"}
 
-redis_c.lpush("target", json.dumps(a))
+# redis_c.lpush("target", json.dumps(a))
+
+redis_c.lpush("list", json.dumps(a))
