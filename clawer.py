3e0a78e33fdd676959324696b670ee3db9ffb2a4
betta-cyber <l21828604@gmail.com>
2020-06-09 16:35:21 +0800

    add clawer

---
+++ clawer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import time
+import json
+from utils import redis_c, load_yaml, md5
+
+
+if __name__ == '__main__':
+
+    while True:
+        for i in ['cac', 'djbh', 'tc260']:
+            data = {'type': i}
+            redis_c.lpush("list", json.dumps(data))
+            time.sleep(300)
+        time.sleep(7200)
